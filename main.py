73c95040179390af5c2c8366ffd5f9127b4c4fa5
nerudxlf <nerudxlf@gmail.com>
2020-10-14 23:56:29 +0600

    5 out of 6 algorithms work

--- main.py
+++ main.py
@@ -3,32 +3,53 @@ import igraph as ig
 import time
 
 
-def community_walktrap_plot(g: object):
+def community_walktrap_plot(g: object) -> dict:
     start_time = time.monotonic()
     wtrap = g.community_walktrap(steps=8)
-    print(f'Прошло {time.monotonic() - start_time}')
+    stop_time = time.monotonic() - start_time
     clust = wtrap.as_clustering()
     max_size = max(clust.sizes())
     len_clust = len(clust.sizes())
     modularity = clust.recalculate_modularity()
-    ig.plot(clust, mark_groups = True)
+    ig.plot(clust, mark_groups=True)
+    return {
+        "размер максимального сообщества": max_size,
+        "число сообществ": len_clust,
+        "модулярность": modularity,
+        "время работы": stop_time
+    }
 
 
-def community_label_propagation_plot(g: object):
+def community_label_propagation_plot(g: object) -> dict:
     start_time = time.monotonic()
     label_prop = g.community_label_propagation()
-    print(f'Прошло {time.monotonic() - start_time}')
+    stop_time = time.monotonic() - start_time
     max_size = max(label_prop.sizes())
     len_clust = len(label_prop.sizes())
     modularity = label_prop.recalculate_modularity()
-    ig.plot(label_prop, mark_groups = True)
+    ig.plot(label_prop, mark_groups=True)
+    return {
+        "размер максимального сообщества": max_size,
+        "число сообществ": len_clust,
+        "модулярность": modularity,
+        "время работы": stop_time
+    }
 
 
-def community_spinglass_plot(g: object):
-    #!!!long counts
+def community_spinglass_plot(g: object) -> dict:
+    start_time = time.monotonic()
     spinglass = g.community_spinglass()
-    print(type(spinglass))
-
+    stop_time = time.monotonic() - start_time
+    max_size = max(spinglass.size())
+    len_clust = len(spinglas.size())
+    modularity = spinglass.recalculate_modularity()
+    ig.plot(spinglass, mark_groups=True)
+    return {
+        "размер максимального сообщества": max_size,
+        "число сообществ": len_clust,
+        "модулярность": modularity,
+        "время работы": stop_time
+    }
 
 
 def community_edge_betweenness_plot(g: object):
@@ -37,37 +58,49 @@ def community_edge_betweenness_plot(g: object):
     print(type(edge_bet))
 
 
-def community_fastgreedy_plot(g: object):
+def community_fastgreedy_plot(g: object) -> dict:
     start_time = time.monotonic()
     fastgreedy = g.community_fastgreedy()
-    print(f'Прошло {time.monotonic() - start_time}')
+    stop_time = time.monotonic() - start_time
     clust = fastgreedy.as_clustering()
     max_size = max(clust.sizes())
     len_clust = len(clust.sizes())
     modularity = clust.recalculate_modularity()
-    ig.plot(clust, mark_groups = True)
+    ig.plot(clust, mark_groups=True)
+    return {
+        "размер максимального сообщества": max_size,
+        "число сообществ": len_clust,
+        "модулярность": modularity,
+        "время работы": stop_time
+    }
 
 
-def community_leading_eigenvector_plot(g: object):
+def community_leading_eigenvector_plot(g: object) -> dict:
     start_time = time.monotonic()
     leading_eigenvector = g.community_leading_eigenvector()
-    print(f'Прошло {time.monotonic() - start_time}')
+    stop_time = time.monotonic() - start_time
     max_size = max(leading_eigenvector.sizes())
     len_clust = len(leading_eigenvector.sizes())
     modularity = leading_eigenvector.recalculate_modularity()
-    ig.plot(leading_eigenvector, mark_groups = True)
-
+    ig.plot(leading_eigenvector, mark_groups=True)
+    return {
+        "размер максимального сообщества": max_size,
+        "число сообществ": len_clust,
+        "модулярность": modularity,
+        "время работы": stop_time
+    }
 
 
 def main():
-    g = ig.Graph.Read_Ncol("facebook_combined.txt",  directed=True).as_undirected()
-    community_walktrap_plot(g)
-    community_label_propagation_plot(g)
+    g = ig.Graph.Read_Ncol("facebook_combined.txt",
+                           directed=True).as_undirected()
+    # community_walktrap_plot(g)
+    # community_label_propagation_plot(g)
     community_edge_betweenness_plot(g)
-    community_spinglass_plot(g)
-    community_fastgreedy_plot(g)
-    #ommunity_leading_eigenvector_plot(g)
+    # community_spinglass_plot(g)
+    # community_fastgreedy_plot(g)
+    # ommunity_leading_eigenvector_plot(g)
 
-    
+
 if __name__ == '__main__':
     main()
